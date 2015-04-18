2eee38c0b4c2c1305661671af0cbbad09ec729e4
ingaizah <edi4yuu@gmail.com>
2015-04-18 19:09:28 +0300

    Exercise 11:Getting User Input

---
+++ ex11.py
@@ -0,0 +1,10 @@
+#Getting User Input
+
+#print('\nHow old are you..>')
+print('--->Hello and Welcome to Our Survey.\n')
+
+age = input('How old are you-->\n')
+height = input('How tall are you(ft)-->\n')
+weight = input('How Much do you weigh(Kg)-->')
+
+print('You are {0} years old,{1} Feet tall,and weigh {2} Kgs.'.format(age,height,weight))
