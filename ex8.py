f8640b58773eadb084562d0077f546e55f6ab413
ingaizah <edi4yuu@gmail.com>
2015-04-18 17:04:45 +0300

    Exercise 8:More Print() Stuff

---
+++ ex8.py
@@ -0,0 +1,11 @@
+__author__ = 'ray'
+#All Python 2
+
+formatter = "%r %r %r %r"
+print(1,2,3,4)
+print('one','two','three','four')
+print(True,False,False,True)
+print(formatter*4)
+print('I had this thing.',
+      'That I am getting this right.',
+      'So I keep on Doing this.')
