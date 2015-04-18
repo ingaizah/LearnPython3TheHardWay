2f34ab7e644e075d88af08b5e4f0b273cf21bc67
ingaizah <edi4yuu@gmail.com>
2015-04-18 17:16:14 +0300

    Exercise 9:Escape Sequence and Printing Multiple Lines

---
+++ ex9.py
@@ -0,0 +1,13 @@
+#Escape Sequence(\n) n Multiple Lines
+
+days='Mon Tue Wed Thur Fri Sat Sun'
+months='Jan\nFeb\nMarch\nApril\nMay\nJune\nJuly\nAug\Sept'
+
+print('\nDays of the week-->',days)
+print('Months;',months)
+
+print("""
+There's something crazy going on here.
+with the '3 Double' Quotes.
+We can type as Much as we like...
+We're Allowed.""")
