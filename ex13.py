14941c2bf05abb433caacc01376ec71203162c34
ingaizah <edi4yuu@gmail.com>
2015-04-18 20:17:06 +0300

    Ex.13:I have a bug.

---
+++ ex13.py
@@ -0,0 +1,11 @@
+#Importing Python Features(Modules)
+#There's a Bug in here
+
+from sys import argv
+
+a,b,c,d = argv
+
+print('First arg =',a)
+print('2nd var =',b)
+print('3rd var =',c)
+print('4th var =',d)
