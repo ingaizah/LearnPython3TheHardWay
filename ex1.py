3a1cc52a8acbef73cf6b6b0e48e2d77ea7183d0e
ingaizah <edi4yuu@gmail.com>
2015-04-18 14:21:26 +0300

    exercise 1

---
+++ ex1.py
@@ -0,0 +1,12 @@
+#Exercise 1;A Good First Program
+# Printing python statements
+
+print('Hello world!!!')
+print('hello ray.')
+print('love you son.')
+print('I like typing this.')
+print('This is so much fun.')
+print('Yay! Printing.')
+print('I\'d much rather \'not\'.') #Note the escape sequencecd quotes
+print('I said I can \'DO\' this.')
+print('Hurray..!!!')
