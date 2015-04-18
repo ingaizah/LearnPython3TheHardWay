a62cd8da7b3b0de27265e836b69d32a2acc97faf
ingaizah <edi4yuu@gmail.com>
2015-04-18 15:01:29 +0300

    Exercise 3; Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,16 @@
+# Numbers and Math
+
+print('I will now count my kittens;')
+
+print('hens',25 + 30 / 6) #Notice Operator Precedence
+print('Roosters',100 -25 * 3 % 4,'\n')# Operators have precedence and do NOT simply
+# execute Sequentially
+
+print('Now I will count the eggs;')
+print(3+2+1-5+4%2-1/4+6,'\n')
+
+print('What\'s 3 + 2?',3+2)
+print('5 - 7 =',5-7,'\n')
+
+print('5 > -2',5>-2)
+print('5 <= 2',5<=2)
