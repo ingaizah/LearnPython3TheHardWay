516050ad0b7c1de9dc3e686bbe630a7ce0fda196
ingaizah <edi4yuu@gmail.com>
2015-04-18 14:34:44 +0300

    Exercise 2;Comments and Pound Characters.

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#Comments and Pound Characters
+
+#A comment,so you can read the program later.
+# Anything after the # is ignored by Python
+
+print('I can have clean code. How?')# Using Comments
+# print('I won\'nt run.') # Hide pieces of (broken)code.
+print('I will run.')
+
