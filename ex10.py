18dbc3a1b3f98879caffb98bc833cc8434848ef6
ingaizah <edi4yuu@gmail.com>
2015-04-18 17:32:12 +0300

    Exercise 10:Escape Sequences \n,\t,\

---
+++ ex10.py
@@ -0,0 +1,17 @@
+#More on Strings
+#NB;the *sign is INSIDE a string so NO Effect
+
+tabby = "\tI'm tabbed in"
+persia = "I'm split\non a line."
+backs = "I'm \\a\\ cat."
+fatty = '''
+I'l do a lit;
+\t* Cat food
+\t* Fish
+\t* Catnip\n\t* Grass
+'''
+
+print(tabby)
+print(persia)
+print(backs)
+print(fatty)
