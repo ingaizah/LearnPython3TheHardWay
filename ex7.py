cc23b7959a0b88dd1587d59559124712678d42e2
ingaizah <edi4yuu@gmail.com>
2015-04-20 13:03:56 +0300

    Exercise 7:Concatenation n Escape Sequence Cancellation

---
+++ ex7.py
@@ -0,0 +1,18 @@
+#Concatenation n Escape Sequence Cancellation
+#Special Use of the '+' Character
+
+print('Mary had a little lamb.')
+print('It\'s fleece {} white as {}'.format('was','snow'))#Syntax..???
+print('And everywhere Mary went.')
+print('.'*10) # Prints 10 Dots
+
+a='che'
+b='ese'
+c='bur'
+d='ger'
+print(a+b,end='') # New-Line x-ter herein Overridden
+print(c+d) # NB:NO QUOTES as they're defined variables
+
+
+
+
